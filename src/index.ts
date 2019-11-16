5045a1d49f2087962d9da7c674376f21318bee78
Lee Jinseop <js428118@gmail.com>
2019-11-16 15:44:25 +0900

    Modified issue tamplate

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import { createIssue } from './github-events'
 function main() {
   const pickedRestaurantNumber = Math.floor(Math.random() * restaurants.length)
   const pickedRestaurant = restaurants[pickedRestaurantNumber] || [];
-  const title = `오늘의 음식점 : ${pickedRestaurant.name}`
-  const menus = pickedRestaurant.menus.map(({ name, price }) => `메뉴명 : ${name}, 가격 : ${price}`)
-  const body = [`메뉴\n${menus.join('\n')}`, `위치 : ${pickedRestaurant.location}`].join('\n')
+  const title = `오늘 점심은 ${pickedRestaurant.name} 어때여?`
+  const menus = pickedRestaurant.menus.map(({ name, price }) => `${name} / ${price}원`)
+  const body = [`## 메뉴\n${menus.join('\n')}`, `## 위치\n${pickedRestaurant.location}`, `## 링크\n${pickedRestaurant.url}`].join('\n')
   createIssue(title, body, ['lunch'])
 }
 
